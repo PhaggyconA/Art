05d8ca0e1671314d0944995cc7389273a1a66335
PhaggyconA <65262035+PhaggyconA@users.noreply.github.com>
2020-05-12 21:52:50 -0500

    Parcial
    profe pongame 5

---
+++ main.js
@@ -0,0 +1,9 @@
+$('#hamburguer').click(function () {
+
+      $('header').toggleClass('open');
+      $('#open').toggleClass('hide');
+      $('#close').toggleClass('show');
+})
+
+
+$('.slick-slider').slick();
